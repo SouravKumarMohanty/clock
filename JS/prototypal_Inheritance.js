58621d773b07864f5772eaa83855ac68aaa31d12
SANTU <mohantysourav201@gmail.com>
2024-10-19 23:22:43 +0530

    updated

---
+++ JS/prototypal_Inheritance.js
@@ -0,0 +1,6 @@
+ function abcd(name,age){
+    this.name = name;
+    this.age= age;
+ }
+const human1 = new abcd("sourav", 563);
+const human2 = new abcd("santu", 171);``
---
+++ JS/script.js
@@ -0,0 +1,72 @@
+/*var a = 10;
+var b = a;
+var ar = [1,2,3,4,5];
+var br = ar;
+var gr = [...ar];
+*/
+//                              if else
+/*
+if(12<13){
+    console.log("Hii");
+}
+else if(12>13){
+    console.log("hlo");
+}
+else if(14>12){
+    console.log("he");
+}
+else{
+    console.log("BYE");
+}
+*/
+
+//                                LOOPS
+/*
+for(Start; end; change){
+
+}
+
+start;
+while(end){
+    change;
+}
+*/
+//                                FAUNCTIONS
+/*
+function abcd(val){
+    console.log(12+val);
+}
+abcd(12);
+*/
+
+/*
+var a= () => {
+    return 20;
+}
+var b = a();
+console.log(b);
+*/
+
+//                                     ARRAY
+
+//var arr = [1,2,function(){},[],"abc"];
+
+//loop in array
+/*
+var arr = [1,2,3,4,5,6];
+arr.forEach(function(val){
+    console.log(val);
+})
+*/
+
+//                                       OBJECT
+
+// var obj = {};
+// var obj2 = new Object();
+
+var obj = {
+    name : "sourav",
+    age : 20,
+    regdno : 563,
+}
+obj.age;
